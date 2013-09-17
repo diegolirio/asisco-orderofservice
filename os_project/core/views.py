99a5b5b3ed8539203cbc095b4e486f67915084ab
Fabiano Góes <fabianogoes@gmail.com>
2013-09-17 18:09:04 -0300

    implementando login

--- os_project/core/views.py
+++ os_project/core/views.py
@@ -1,8 +1,14 @@
 # coding: utf-8
-from django.shortcuts import render
-from models import OrdemServico
+from django.shortcuts import render, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+#from models import OrdemServico
 
+@login_required(login_url='/login/')
 def home(request):
-    #from django.http import HttpResponse
     context = None #{'lista': OrdemServico.objects.all(),}
     return render(request, 'index.html', context)
+
+def orderservice(request, pk):
+    context = {}
+    return render(request, 'orderservice.html', context)
--- os_project/urls.py
+++ os_project/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
-
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'os_project.core.views.home', name='home'),
-    # url(r'^os_project/', include('os_project.foo.urls')),
+    url(r'^login/', "django.contrib.auth.views.login", {"template_name": "login.html"}, name='login'),
+    url(r'^logout/', "django.contrib.auth.views.logout_then_login", {'login_url': '/login/'}, name='logout'),    
+
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^', include(admin.site.urls)),
+    
+    url(r'^orderservice/(?P<pk>\d+)/$', 'os_project.core.views.orderservice', name='orderservice'),
 )
